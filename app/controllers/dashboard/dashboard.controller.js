03ea366f073f0e5d86df4027f21a03b12121d773
Edgardo <jemedina.96@gmail.com>
2017-12-14 17:39:55 -0600

    dashboard

--- app/controllers/dashboard/dashboard.controller.js
+++ app/controllers/dashboard/dashboard.controller.js
@@ -1,11 +1,19 @@
 
-var dashboardController = function($scope) {
-    $scope.saludo = "Hola soy el dashboard, quizá abrá aqui algo, quizá no, quien sabe :)";
+var dashboardController = function($scope, $http) {
+   
 
-    $scope.decirHola = function() {
-        alert("Hola :)");
-    }
+    $scope.cargar = function() {
+        $http({
+			url: 'api/obtenerProductosAsurtir.php',
+            method: 'POST',
+		}).then(function(resp) {
+            $scope.surtirlista=resp.data; 
+		},function(err) {
+			swal(err.data.msg, { icon: "error" } );			
+		})
+    } 
+    $scope.cargar()
 }
 
-dashboardController.$inject = ['$scope'];
+dashboardController.$inject = ['$scope','$http'];
 app.controller('dashboardController', dashboardController);
