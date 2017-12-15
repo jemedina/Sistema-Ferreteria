b34097dd1d6ca8129d451f72ada894c90fc7a46c
andresdeluna27 <paco_pokemaster12@hotmail.com>
2017-12-14 19:48:30 -0600

    obtener nombre producto
    posible cambio para ver si regresa el nombre del producto en devoluciones

--- api/obtenerProductosParaDevolucion.php
+++ api/obtenerProductosParaDevolucion.php
@@ -1,6 +1,6 @@
 <?php
     require_once 'config.php';
-    $sql="select codigo, marca, precio_vendido, cantidad, unidades from producto_venta where concat(no_venta, ' ', fecha)='$request->id'";
+    $sql="select a.codigo, a.marca, a.precio_vendido, a.cantidad, a.unidades, b.nombre from producto_venta a,producto b where concat(no_venta, ' ', fecha)='$request->id' and a.codigo=b.codigo";
     $result = $con->query($sql);
 	echoMysqlResults($result);
  
